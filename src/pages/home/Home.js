68d516972d7c6bdce78eaa6f288046c6a30ea839
unknown <rjohnquia@gmail.com>
2022-05-04 10:03:54 +0800

    fixe used but not used issue

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Announcement from '../../components/announcement/Announcement'
 import Charges from '../../components/charges/Charges'
 import Hero from '../../components/hero/Hero' 
-import MainNavbar from '../../components/mainNavbar/MainNavbar'
+
 
 import Footer from '../../components/footer/Footer'
 
